919ddf0980b352e3ccdc8f1ed4244ff59a3c40f6
Mark-Kohrman <mark.kohrman@gmail.com>
2020-12-20 20:25:32 -0600

    show action initial setup

---
+++ src/views/ShowMovies.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="movies-show">
+    <h1>{{ movie.title}}</h1>
+    <h2> Director: {{ movie.director}}</h2>
+    <h2> Year: {{ movie.release_year }}</h2>
+    <p>{{ movie.description }}</p>
+    <img v-bind:src="movie.img_url">
+
+
+
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      message: "This is the show page!!!",
+      movie: {},
+    };
+  },
+  created: function () {
+    axios.get("/api/movies/1").then((response) => {
+      console.log("show me the movies", response);
+      this.movie = response.data;
+    });
+  },
+  methods: {},
+};
+</script>
